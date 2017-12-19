dcae1695987756968f865bf66bd2998b79ce18a6
Jessica Liu <hepu24@gmail.com>
2017-12-19 00:28:14 -0500

    initial commit, setup react project with webpack

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+// import MapGoogle from './MapGoogle';
+// import JsonChart from './JsonChart';
+
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h2>HELLO</h2>
+            </div>
+        );
+    }
+}
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+/*
+    ./webpack.config.js
+*/
+
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.jsx',
+    output: {
+        path: path.resolve('dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+        { 
+            test: /\.js?$/, 
+            loader: 'babel-loader', 
+            exclude: /node_modules/,
+            query: {
+                "presets": ["es2015", "stage-0"]
+            }
+        },
+        { 
+            test: /\.jsx?$/, 
+            loader: 'babel-loader', 
+            exclude: /node_modules/,
+            query:{
+                "presets": ["react", "es2015", "stage-0"]
+            } 
+        }
+        ]
+    }
+}
