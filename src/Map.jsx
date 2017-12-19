665c1d0b0946d027a56b3c2a68a94561c2bb8b43
Jessica Liu <hepu24@gmail.com>
2017-12-19 16:51:55 -0500

    parsed json data using cors-anywhere, added markers

--- src/Map.jsx
+++ src/Map.jsx
@@ -7,17 +7,73 @@ class Map extends Component {
     this.state = {};
   }
 
-  componentDidMount() {
-    // Connect the initMap() function within this class to the global window context,
-    window.initMap = this.initMap;
-  }
+    componentDidMount() {
+        // Connect the initMap() function within this class to the global window context
+        window.initMap = this.initMap;
+    }
 
-  initMap = () => {
-    this.map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: 43.6532, lng: -79.3832},
-      zoom: 8
-    });
-  }
+    initMap = () => {
+        this.generateMap();
+    }
+
+    // read all meter's Geo Data from API
+    // initialize the map with one Geo Data
+    // loop through the Geo Data and call the addMarker function to add each meter onto the map
+
+    generateMap = () => {
+        // fetch data from API, remove CORS
+        fetch(
+            // use cors-anywhere to solve the cors issue
+            'https://cors-anywhere.herokuapp.com/https://lilcortexbucket.blob.core.windows.net/public/meters.json',
+        ).then(response => {
+            console.log(response);
+            if (response.status !== 200) {
+                console.log('ERROR - failed to get json data');
+                return;
+            }
+            return response.json();
+        }).then(results =>{
+            console.log(results);
+
+            // loop through the data to obatin the avg lat & lng values as center for the map initialization
+            let numMeters = 0;
+            let sumlat = 0;
+            let sumlng = 0;
+
+            for (let meterID in results){
+                sumlat += parseFloat(results[meterID].meter_latitude);
+                sumlng += parseFloat(results[meterID].meter_longitude);
+                numMeters ++;
+            }
+
+            console.log(sumlat/numMeters, sumlng/numMeters, numMeters);
+
+            // initialize map with averaged value of center
+            this.map = new google.maps.Map(document.getElementById('map'), {
+                center: {lat: sumlat/numMeters, lng: sumlng/numMeters},
+                zoom: 10
+            });
+    
+            // add meter markers to map
+            for (let meterID in results){
+                this.addMarker(meterID, results[meterID]);
+            }
+        }). catch(err => {
+            console.log(err);
+        })
+    }
+
+    // function to add a meter location to the map as a marker
+    addMarker = (meterID, meterData) => {
+        new google.maps.Marker({
+            position: {
+                lat: parseFloat(meterData.meter_latitude),
+                lng: parseFloat(meterData.meter_longitude)
+            },
+            label: meterID,
+            map: this.map
+        })
+    }
 
   render() {
     return (
